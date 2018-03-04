462d24f7d4d829cab7878d2c55431a4cda1336b0
Bruno Coitos <b.coitos@accenture.com>
2018-03-04 23:58:22 +0000

    New activity created to be used after language selected

--- app/src/main/java/com/iscte/dam/MainActivity.java
+++ app/src/main/java/com/iscte/dam/MainActivity.java
@@ -1,13 +1,30 @@
 package com.iscte.dam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SELECTED_LANGUAGE = "com.iscte.dam.MainActivity.LANGUAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectLanguage(View view) {
+        int viewID = view.getId();
+        String resourceName = getResources().getResourceEntryName(viewID);
+        Log.d("MainAtivitityLog",resourceName);
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        intent.putExtra(SELECTED_LANGUAGE, resourceName);
+        startActivity(intent);
     }
 }
